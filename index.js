93703bb438d702bb16f302d2cacfe6da3d1a65f1
Krisz <41510145+NKkrisz@users.noreply.github.com>
2023-05-25 22:50:38 +0200

    Basic logic

--- index.js
+++ index.js
@@ -19,7 +19,39 @@ const cards = {
     "Unity" : "./img/icons8-unity.svg"
 }
 
-for(let item in cards){
-    console.log(item, cards[item]);
-    cardsContainer.innerHTML += `<div class="bg-slate-400 border border-slate-950 md:w-32 md:h-32 w-20 h-20 p-2 text-black rounded-xl flex justify-center"><img src="${cards[item]}" alt=""></div>`
+function randomize(){
+    let cardPairs = 6;
+    let alreadyChosen = [] 
+    let rnd;
+    
+    for(let i = 0; i < cardPairs; i++){
+        while (true){
+            rnd = Math.floor(Math.random() * Object.keys(cards).length);
+            if(!alreadyChosen.includes(rnd)){
+                alreadyChosen.push(rnd)
+                break;
+            }
+        }
+        
+        cardsContainer.innerHTML += `<div onclick=check(this) class="game${Object.keys(cards)[rnd]} cursor-pointer bg-slate-400 border border-slate-950 md:w-32 md:h-32 w-20 h-20 p-2 text-black rounded-xl flex justify-center"><img src="${cards[Object.keys(cards)[rnd]]}" alt=""></div>`
+        cardsContainer.innerHTML += `<div onclick=check(this) class="game${Object.keys(cards)[rnd]} cursor-pointer bg-slate-400 border border-slate-950 md:w-32 md:h-32 w-20 h-20 p-2 text-black rounded-xl flex justify-center text-center">${Object.keys(cards)[rnd]}</div>`
+    }
+    
+    alreadyChosen = [];
+}
+
+randomize()
+
+let flipped = [];
+
+function check(obj){
+    flipped.push(obj.classList[0])
+    if(flipped.length === 2){
+        if(flipped[0]===flipped[1]){
+            console.log("win");
+        } else {
+            console.log("lose");
+        }
+        flipped = [];
+    }
 }
